b4bcc0f569bd428dad272072ea3ef9c768963cd6
sabirkazi135 <sabirkazi135@gmail.com>
2025-05-20 21:16:18 +0530

    commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import "./index.css";
+function App() {
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <h1 className="text-2xl font-semibold">Hello, World</h1>
+    </div>
+  );
+}
+
+export default App;
