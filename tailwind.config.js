24e3fe8ab635121592e4d3f80d77a1c123659ce9
sabirkazi135 <sabirkazi135@gmail.com>
2025-05-21 20:23:49 +0530

    commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,11 @@ export default {
   theme: {
     extend: {
       colors: {
-        brand: '#0fa9e6',
-        'brand-light': '#3fbaeb',
-        'brand-dark': '#0c87b8',
+        brand: {
+          DEFAULT: '#0fa9e6',
+          light: '#3fbaeb',
+          dark: '#0c87b8',
+        },
       },
     },
   },
