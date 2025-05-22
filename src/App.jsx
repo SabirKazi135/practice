246fad6a122749859c0b930edd419186125cb0f2
sabirkazi135 <sabirkazi135@gmail.com>
2025-05-22 18:40:23 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
 import React from 'react';
-import Index from './Index';
-import Destination from './Destination';
 
 export default function App() {
   return (
     <div>
-      <Index />
-      <Destination />
+      <h1>Hello World</h1>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,18 +2,7 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {
-      colors: {
-        brand: {
-          DEFAULT: '#0fa9e6',
-          light: '#3fbaeb',
-          dark: '#0c87b8',
-        },
-      },
-      fontFamily: {
-        headline: 'Poppins, sans-serif',
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
