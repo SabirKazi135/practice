3ada50fe4306b8b6d11cd20b1c63078a01baf70e
sabirkazi135 <sabirkazi135@gmail.com>
2025-05-26 16:55:26 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,9 @@ export default function App() {
     <div>
       <div class="flex min-h-screen items-center justify-center bg-gray-50 px-16">
         <div class="relative w-full max-w-lg">
-          <div class="absolute -left-4 top-0 h-72 w-72 animate-blo rounded-full bg-purple-300 opacity-100 mix-blend-multiply filter"></div>
-          <div class="animation-delay-2000 absolute -right-4 top-0 h-72 w-72 animate-blo rounded-full bg-yellow-300 opacity-100 mix-blend-multiply filter"></div>
-          <div class="animation-delay-4000 absolute -bottom-8 left-20 h-72 w-72 animate-blo rounded-full bg-pink-300 opacity-100 mix-blend-multiply filter"></div>
+          <div class="absolute -left-4 top-0 h-72 w-72 animate-blob rounded-full bg-purple-300 opacity-70 mix-blend-multiply blur-xl filter"></div>
+          <div class="animation-delay-2000 absolute -right-4 top-0 h-72 w-72 animate-blob rounded-full bg-yellow-300 opacity-70 mix-blend-multiply blur-xl filter"></div>
+          <div class="animation-delay-4000 absolute -bottom-8 left-20 h-72 w-72 animate-blob rounded-full bg-pink-300 opacity-70 mix-blend-multiply blur-xl filter"></div>
           <div class="relative m-8 space-y-4">
             <div class="flex items-center justify-between space-x-8 rounded-lg bg-white p-5">
               <div class="flex-1">
