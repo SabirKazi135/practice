3471f099e969a3fd5a2764f8b55d03971f01e688
sabirkazi135 <sabirkazi135@gmail.com>
2025-05-26 20:15:11 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,22 @@ import React from 'react';
 
 export default function App() {
   return (
-    <div className="h-screen bg-gray-900">
-      <h1 className="text-center text-4xl font-bold text-gray-200">
-        Hello, Sabir. Now come on make projects
-      </h1>
+    <div className="font-poppins flex h-screen items-center bg-[linear-gradient(-200deg,_#1212b7_0,_#accbe5_100%)]">
+      <div className="pl-36 pr-8">
+        <header className="">
+          <h1 className="font-unbounded text-[40px] font-bold text-white">
+            JavaScript
+          </h1>
+          <h1 className="font-unbounded text-[40px] font-bold text-[#a2ff86]">
+            Age Calculator
+          </h1>
+        </header>
+        <section>
+          <input type="date" name="" id="" />
+          <button>Calculate</button>
+        </section>
+        <p></p>
+      </div>
     </div>
   );
 }
