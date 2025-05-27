42c31da254aa4009efd6a42a0e2fcb5bd59a0cc6
sabirkazi135 <sabirkazi135@gmail.com>
2025-05-27 10:42:42 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,17 @@ export default function App() {
   const [input, setInput] = useState(null);
 
   function onClick() {
-    setInput(null)
+    setInput(null);
   }
 
   function deleteChar() {
     if (input.length > 0) {
-      
-      setInput(input.substring(0, input.length - 1))
+      setInput(input.substring(0, input.length - 1));
     }
+  }
+
+  function addChar(char) {
+    setInput((prev) => prev + char);
   }
   return (
     <div>
@@ -20,23 +23,23 @@ export default function App() {
         <section>
           <button onClick={clear}>AC</button>
           <button onClick={deleteChar}>DE</button>
-          <button onClick={}>.</button>
-          <button onClick={}>/</button>
-          <button onClick={}>7</button>
-          <button onClick={}>8</button>
-          <button onClick={}>9</button>
-          <button onClick={}>*</button>
-          <button onClick={}>4</button>
-          <button onClick={}>5</button>
-          <button onClick={}>6</button>
-          <button onClick={}>-</button>
-          <button onClick={}>1</button>
-          <button onClick={}>2</button>
-          <button onClick={}>3</button>
-          <button onClick={}>+</button>
-          <button onClick={}>00</button>
-          <button onClick={}>0</button>
-          <button onClick={}>=</button>
+          <button onClick={() => addChar('.')}>.</button>
+          <button onClick={() => addChar('/')}>/</button>
+          <button onClick={() => addChar('7')}>7</button>
+          <button onClick={() => addChar('8')}>8</button>
+          <button onClick={() => addChar('9')}>9</button>
+          <button onClick={() => addChar('*')}>*</button>
+          <button onClick={() => addChar('4')}>4</button>
+          <button onClick={() => addChar('5')}>5</button>
+          <button onClick={() => addChar('6')}>6</button>
+          <button onClick={() => addChar('-')}>-</button>
+          <button onClick={() => addChar('1')}>1</button>
+          <button onClick={() => addChar('2')}>2</button>
+          <button onClick={() => addChar('3')}>3</button>
+          <button onClick={() => addChar('+')}>+</button>
+          <button onClick={() => addChar('00')}>00</button>
+          <button onClick={() => addChar('0')}>0</button>
+          <button onClick={() => addChar('=')}>=</button>
         </section>
       </main>
     </div>
