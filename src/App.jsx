74d87c3987d77e1eb2287747dd5ad91c8350e2de
sabirkazi135 <sabirkazi135@gmail.com>
2025-05-28 17:40:07 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,41 @@
 import React, { useState } from 'react';
 
 export default function App() {
+  const [image, setImage] = useState(null);
   return (
-    <div>
-      <div></div>
-      <div></div>
+    <div className="mt-32 flex justify-center">
+      <section
+        className="h-[50px] w-[416px] rounded-[20px] p-8"
+        style={{
+          backgroundImage: 'linear-gradient(-45deg, #46C2CB 20%, #E5E5CB 100%)',
+        }}
+      >
+        <input type="text" />
+        <button></button>
+        <section>
+          <header>
+            <div>
+              <span></span>
+              <span></span>
+              <span></span>
+            </div>
+            <div style={{ backgroundImage: { image } }}></div>
+          </header>
+          <section>
+            <div>
+              <span></span>
+              <span></span>
+              <span></span>
+            </div>
+            <div>
+              <span></span>
+              <span></span>
+              <span></span>
+            </div>
+          </section>
+          <p></p>
+        </section>
+      </section>
     </div>
   );
 }
