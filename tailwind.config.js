4dfe458eca3b574aa7d6bf132d4ca120f36e4489
sabirkazi135 <sabirkazi135@gmail.com>
2025-05-28 18:17:08 +0530

    commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,27 +2,7 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {
-      colors: {
-        primary: '#2d4356', // Blue
-        secondary: '#dff7ff', // Gray
-      },
-    },
+    extend: {},
   },
-  plugins: [
-    function ({ addUtilities }) {
-      const newUtilities = {
-        '.hide-scrollbar': {
-          /* Chrome, Safari and Edge */
-          '&::-webkit-scrollbar': {
-            display: 'none',
-          },
-          /* Firefox */
-          'scrollbar-width': 'none',
-          '-ms-overflow-style': 'none',
-        },
-      };
-      addUtilities(newUtilities, ['responsive']);
-    },
-  ],
+  plugins: [],
 };
