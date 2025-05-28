40c0d2000bf4d1d3cfcc08b5d57d02dc54baa269
sabirkazi135 <sabirkazi135@gmail.com>
2025-05-28 20:20:26 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,45 @@
 import React, { useState } from 'react';
 
 export default function App() {
+  const [taskList, setTaskList] = useState([
+    { title: 'Task 1', done: false },
+    { title: 'Task 2', done: false },
+    { title: 'Task 3', done: false },
+  ]);
+
   return (
     <div className="flex h-screen items-center justify-center bg-[#4A55A2]">
-      <div className="h-[90%] rounded-[32px] bg-white px-4 py-8 shadow-2xl">
+      <div className="h-[90%] w-[448px] space-y-6 rounded-[32px] bg-white px-4 py-8 shadow-2xl">
         <div className="text-[32px] font-bold text-[#212121]">To-Do list</div>
         <form className="w-full">
-          <nav className="w-full rounded-[48px] bg-[#eee] pl-4">
+          <nav className="h- flex h-12 w-full items-center justify-between rounded-[48px] bg-[#eee] pl-4">
             <input
               type="text"
-              className="h-5 bg-transparent px-[2px] py-[1px] outline-none"
+              placeholder="I need to do..."
+              className="h-5 bg-transparent px-[2px] py-[1px] text-base text-black outline-none"
             />
-            <button>Add</button>
+            <button className="h-12 rounded-[48px] bg-[#F24C3D] px-8 text-center font-bold text-[#eee]">
+              Add
+            </button>
           </nav>
         </form>
-        <div className=""></div>
+        <div className="flex flex-col">
+          {taskList.map((task, index) => {
+            return (
+              <div key={index} className="flex">
+                <button>
+                  {task.done ? (
+                    <i className="fa-solid fa-circle-check text-[#F24C3D]"></i>
+                  ) : (
+                    <i className="fa-regular fa-circle text-gray-400"></i>
+                  )}
+                </button>
+                <p>{task.title}</p>
+                <button>*</button>
+              </div>
+            );
+          })}
+        </div>
       </div>
     </div>
   );
