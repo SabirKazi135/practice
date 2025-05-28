f00a3ea8f658296c87744f52c03359382fb3a473
sabirkazi135 <sabirkazi135@gmail.com>
2025-05-28 17:57:23 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,23 @@ import React, { useState } from 'react';
 export default function App() {
   const [image, setImage] = useState(null);
   return (
-    <div className="mt-32 flex justify-center">
+    <div className="mt-32 flex items-center justify-center">
       <section
-        className="h-[50px] w-[416px] rounded-[20px] p-8"
+        className="rounded-[20px] p-8"
         style={{
           backgroundImage: 'linear-gradient(-45deg, #46C2CB 20%, #E5E5CB 100%)',
         }}
       >
-        <input type="text" />
-        <button></button>
+        <section className="flex gap-5">
+          <input
+            placeholder="enter city name"
+            type="text"
+            className="flex h-[50px] w-[306px] items-center rounded-[50px] px-5 text-[20px] text-black outline-none"
+          />
+          <button className="h-[50px] w-[50px] rounded-[50%] bg-white px-[6px] py-[1px] text-black">
+            <i class="fa-solid fa-magnifying-glass"></i>
+          </button>
+        </section>
         <section>
           <header>
             <div>
