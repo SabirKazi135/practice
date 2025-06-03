4db7819da85a786764a098208f6e8fd222371ee7
sabirkazi135 <sabirkazi135@gmail.com>
2025-06-03 17:26:29 +0530

    commit

---
+++ src/Header.jsx
@@ -0,0 +1,11 @@
+function Header() {
+  return (
+    <div className="h-12 max-w-[992px] px-8 py-4">
+      <div className="">
+        <img src="logo.png" alt="" />
+      </div>
+    </div>
+  );
+}
+
+export default Header;
