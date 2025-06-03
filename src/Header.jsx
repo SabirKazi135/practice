0289754faa0b34cada7bde0bb153f824302d86d4
sabirkazi135 <sabirkazi135@gmail.com>
2025-06-03 17:28:05 +0530

    commit

--- src/Header.jsx
+++ src/Header.jsx
@@ -3,6 +3,17 @@ function Header() {
     <div className="h-12 max-w-[992px] px-8 py-4">
       <div className="">
         <img src="logo.png" alt="" />
+        <nav>
+          <div>
+            <ul>
+              <li>Market</li>
+              <li>Features</li>
+              <li>White Papers</li>
+              <li>About Us</li>
+            </ul>
+          </div>
+          <div>EN</div>
+        </nav>
       </div>
     </div>
   );
