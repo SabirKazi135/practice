64d6f2f102cc1cd2d1890e8ba9c023a162b57ec2
sabirkazi135 <sabirkazi135@gmail.com>
2025-06-03 17:31:24 +0530

    commit

--- src/Header.jsx
+++ src/Header.jsx
@@ -2,15 +2,14 @@ function Header() {
   return (
     <div className="h-12 max-w-[992px] px-8 py-4">
       <div className="flex">
-        <img src="logo.png" alt="" />
-        <nav className="flex">
+        <img className="w-12" src="logo.png" alt="" />
+        <nav className="flex justify-between">
           <ul className="flex">
             <li>Market</li>
             <li>Features</li>
             <li>White Papers</li>
             <li>About Us</li>
           </ul>
-
           <div>EN</div>
         </nav>
       </div>
