34622cf14afa6ec2f13d453411e8a037e0b7333a
sabirkazi135 <sabirkazi135@gmail.com>
2025-06-06 20:39:48 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,7 @@
-import Header from './Header';
-
 export default function App() {
   return (
-    <div className="bg-deepNight flex h-screen justify-center">
-      <div className="text-creamWhite w-full max-w-[992px]">
-        <Header />
-      </div>
+    <div>
+      <h1>Hello</h1>
     </div>
   );
 }
