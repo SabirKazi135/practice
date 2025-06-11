a8e9890210bccb8645876823cb53ea24f8707788
sabir kazi <sabirkazi135@gmail.com>
2025-06-11 17:24:37 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
 export default function App() {
   return (
-    <div className="flex h-screen flex-col space-y-4 bg-slate-800 p-8">
-      <button className="rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-600 active:bg-blue-700">
-        Click Me
-      </button>
+    <div className="h-screen bg-slate-900 p-6 text-white">
+      <div className="space-y-6">
+        <h1 className="text-xl font-bold">Active State Example</h1>
+
+        <button className="rounded bg-purple-500 px-6 py-2 text-white transition-all duration-200 hover:bg-purple-600 active:bg-purple-700">
+          Press Me
+        </button>
+
+        <button className="rounded bg-green-500 px-6 py-2 text-white transition-transform duration-150 hover:scale-105 active:scale-95">
+          Scale On Active
+        </button>
+      </div>
+      <div className="space-y-6">
+        <button className="t rounded bg-purple-500 px-6 py-2">Press Me</button>
+
+        <button className="ransition-transform duration-150 hover:scale-105 active:scale-95">
+          Scale On Active
+        </button>
+      </div>
     </div>
   );
 }
