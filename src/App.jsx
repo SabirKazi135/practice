e0840a44467627e92bc520e98ef524a49b8cad66
sabir kazi <sabirkazi135@gmail.com>
2025-06-11 16:38:20 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,23 @@
 export default function App() {
   return (
     <div className="h-screen space-y-4 bg-slate-800 p-8">
-      <input
-        type="text"
-        class="rounded border border-gray-300 p-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
-      />
-      <br />
-
-      <input
-        type="text"
-        className="rounded border border-gray-300 focus:outline-none"
-      />
+      <div>
+        <div class="flex flex-col gap-4 p-4">
+          <input
+            type="text"
+            placeholder="Your name"
+            class="rounded border p-2 focus:border-purple-500 focus:ring-4 focus:ring-purple-300"
+          />
+          <input
+            type="email"
+            placeholder="Email"
+            class="rounded border p-2 focus:outline-none focus:ring-2 focus:ring-red-400"
+          />
+        </div>
+      </div>
+      <div>
+        
+      </div>
     </div>
   );
 }
