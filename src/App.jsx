435c32e7e684229489f6f5b19373a478b940c589
sabir kazi <sabirkazi135@gmail.com>
2025-06-11 17:35:11 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,34 @@
+import { useState } from 'react';
+
 export default function App() {
+  const [isDisabled, setIsDisabled] = useState(true);
+
   return (
     <div className="h-screen bg-slate-900 p-6 text-white">
       <div className="space-y-6">
-        <h1 className="text-xl font-bold">Active State Example</h1>
+        <h1 className="text-xl font-bold">Disabled State Example</h1>
 
-        <button className="rounded bg-purple-500 px-6 py-2 text-white transition-all duration-200 hover:bg-purple-600 active:bg-purple-700">
-          Press Me
+        <button
+          className="rounded bg-blue-500 px-4 py-2 transition-all disabled:cursor-not-allowed disabled:bg-gray-500"
+          disabled={isDisabled}
+        >
+          Submit
         </button>
 
-        <button className="rounded bg-green-500 px-6 py-2 text-white transition-transform duration-150 hover:scale-105 active:scale-95">
-          Scale On Active
+        <button
+          onClick={() => setIsDisabled(!isDisabled)}
+          className="rounded bg-green-500 px-4 py-2"
+        >
+          Toggle Disabled
         </button>
       </div>
       <div className="space-y-6">
-        <button className="t rounded bg-purple-500 px-6 py-2 transition-all duration-200 hover:bg-purple-600 active:bg-purple-700">
-          Press Me
-        </button>
-
-        <button className="rounded bg-green-500 px-6 py-2 transition-transform duration-150 hover:scale-105 hover:bg-green-600 active:scale-95 active:bg-green-700">
-          Scale On Active
+        <h1>Disable State Example</h1>
+        <button
+          disabled={false}
+          className="rounded bg-blue-500 px-4 py-2 transition-all disabled:cursor-not-allowed disabled:bg-gray-500"
+        >
+          Submit
         </button>
       </div>
     </div>
