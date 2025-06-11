f7100069062ba2463d8b42b314bcd08dc1f9b58a
sabir kazi <sabirkazi135@gmail.com>
2025-06-11 16:12:10 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import { useState } from 'react';
-
 export default function App() {
   return (
     <div>
-      <h1>Hello</h1>
-      <p>Sabir Kazi</p>
+      <h1 className="bg-gray-500 p-4 text-white">Hello</h1>
+      <button className="rounded bg-blue-600 p-2 text-white lg:hover:bg-indigo-500 dark:hover:bg-indigo-600">
+        Hello Sabir
+      </button>
     </div>
   );
 }
