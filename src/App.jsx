a85a4b846691ca40f7fe84b6ab273ed1f523069b
sabir kazi <sabirkazi135@gmail.com>
2025-06-11 19:14:16 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,16 @@ export default function App() {
             placeholder="you@example.com"
           />
         </div>
+        <div className="rounded-lg border border-gray-500 bg-slate-800 p-6 focus-within:border-green-500 focus-within:ring-2 focus-within:ring-green-300">
+          <label htmlFor="" className="block text-sm font-medium text-gray-300">
+            Email Address
+          </label>
+          <input
+            type="email"
+            className="mt-1 w-full rounded border border-gray-700 bg-slate-700 p-2 outline-none focus:border-green-400 focus:ring-2 focus:ring-green-200"
+            placeholder="you@example.com"
+          />
+        </div>
       </div>
     </div>
   );
