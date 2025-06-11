cbaaa673b23b541e7af38c4fcc315b026992c931
sabir kazi <sabirkazi135@gmail.com>
2025-06-11 17:11:13 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,9 @@
 export default function App() {
   return (
     <div className="flex h-screen flex-col space-y-4 bg-slate-800 p-8">
-      <input
-        type="text"
-        placeholder="Test hover + focus"
-        className="rounded border p-2 text-white hover:bg-slate-700 focus:border-green-500 focus:ring-2 focus:ring-green-300"
-      />
-      <input
-        type="text"
-        placeholder="Test hover + focus"
-        className="rounded border p-2 text-white hover:bg-slate-700 focus:ring-2 focus:ring-green-300"
-      />
+      <button className="rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-600 active:bg-blue-700">
+        Click Me
+      </button>
     </div>
   );
 }
