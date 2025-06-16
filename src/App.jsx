29164bbe8459e431d7a9f29f160775b728332094
sabir kazi <sabirkazi135@gmail.com>
2025-06-16 21:20:05 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,10 @@
 export default function App() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-[] bg-slate-900 p-10 text-white">
-      <ul className="w-40 bg-slate-700 p-3">
-        {[...Array(5)].map((_, index) => (
-          <li
-            key={index}
-            className="bg-white px-4 py-2 text-purple-600 outline-none hover:bg-yellow-300 focus:bg-green-400 active:bg-red-500"
-            tabIndex={0} // for focus to work
-          >
-            list {index + 1}
-          </li>
-        ))}
-      </ul>
-
-      <div className="rounded-[10px] p-4 text-white">Random BG Color</div>
+    <div className="flex min-h-screen items-center justify-center bg-[] bg-slate-900 p-10">
+      <form className="space-y-4">
+        <input type="email" name="" id="" className="px-4 py-2 "/>
+        <input type="number" name="" id="" className="px-4 py-2 "/>
+      </form>
     </div>
   );
 }
-
