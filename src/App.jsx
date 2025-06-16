61665aa97dead2dab41781b6e1da98e02d1e3909
sabir kazi <sabirkazi135@gmail.com>
2025-06-16 21:22:32 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 export default function App() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-[] bg-slate-900 p-10">
-      <form className="space-y-4">
-        <input type="email" name="" id="" className="px-4 py-2 "/>
-        <input type="number" name="" id="" className="px-4 py-2 "/>
+      <form className="space-x-4 space-y-4">
+        <input
+          type="email"
+          name=""
+          id=""
+          className="px-4 py-2 required:border-[2px] required:border-red-500"
+          required
+        />
+        <input type="number" name="" id="" className="px-4 py-2" />
       </form>
     </div>
   );
