e0669f51022a496f27668c5b4665253cbf844ec9
sabir kazi <sabirkazi135@gmail.com>
2025-06-16 21:28:39 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
+import { useState } from 'react';
 export default function App() {
+  const [touched, setTouched] = useState(false);
   return (
     <div className="flex min-h-screen items-center justify-center bg-[] bg-slate-900 p-10">
       <form className="space-x-4 space-y-4">
         <input
           type="email"
-          name=""
-          id=""
-          className="px-4 py-2 required:border-[2px] required:border-red-500"
           required
+          onBlur={() => setTouched(true)} // mark as touched
+          className={`border-2 px-4 py-2 ${
+            touched ? 'invalid:border-red-500' : ''
+          }`}
         />
-        <input type="number" name="" id="" className="px-4 py-2" />
       </form>
     </div>
   );
