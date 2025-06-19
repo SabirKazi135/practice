5a7002d94c9cc04ec5099ac258ce9c0b5ebebad8
sabir kazi <sabirkazi135@gmail.com>
2025-06-19 20:00:50 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,8 @@ import { useState } from 'react';
 export default function App() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-gray-500 p-10">
-      <p className="before:content-['✅ '] relative before:mr-2 before:text-red-500">
-        
+      <p className="relative before:mr-2 before:text-green-600 before:content-['✅']">
+        Task Completed
       </p>
     </div>
   );
