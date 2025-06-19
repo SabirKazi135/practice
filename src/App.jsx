b2355daaa92a5ef47f83cd68a914a92c778fb8ff
sabir kazi <sabirkazi135@gmail.com>
2025-06-19 20:04:38 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
-import { useState } from 'react';
-
 export default function App() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-gray-500 p-10">
-      <p className="relative before:mr-2 before:text-green-600 before:content-['✅']">
-        Task Completed
-      </p>
+    <div className="flex min-h-screen flex-col items-center justify-center gap-4 bg-gray-800 p-10">
+      <button className="relative rounded bg-blue-600 px-5 py-3 text-white transition-all duration-300 after:ml-2 after:inline-block after:content-['⬇️'] hover:after:translate-x-1">
+        Download Now
+      </button>
+      <button className="">Download Now</button>
     </div>
   );
 }
