9dd4d26a417db346563848530a16f26e333628e4
sabir kazi <sabirkazi135@gmail.com>
2025-06-20 20:59:54 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
 export default function App() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center gap-12 bg-gray-400 p-10 text-white">
-      <div className="container mx-auto bg-blue-200 p-4">
-        <p>This box is centered and width is responsive.</p>
+      {/* ✅ WITH container */}
+      <div className="container mx-auto bg-blue-200 p-4 text-black">
+        <p>
+          This box is centered and width is responsive (using{' '}
+          <code>container</code>).
+        </p>
       </div>
-      <div className="bg-red-200 p-4">
-        <p>This box stretches full width of the screen.</p>
+
+      {/* ❌ WITHOUT container */}
+      <div className="w-full bg-red-200 p-4 text-black">
+        <p>
+          This box stretches full width of the screen (no <code>container</code>
+          ).
+        </p>
       </div>
     </div>
   );
