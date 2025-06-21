041c894a9d09a339c06c418738ff4a30f6c97f88
sabir kazi <sabirkazi135@gmail.com>
2025-06-21 18:58:37 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 export default function App() {
   return (
-    <div className="min-h-screen space-y-6 bg-white p-10 text-center dark:bg-black">
-      <h1>Custom Themed Text</h1>
+    <div className="min-h-screen space-y-6 bg-slate-100 p-10 text-center">
+      <h1 className="text-shadow-lg hover:text-shadow-lg lg:text-shadow-lg">
+        Glowing Text
+      </h1>
     </div>
   );
 }
