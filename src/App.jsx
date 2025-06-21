5a0621d0fdff93671df27b42872283ff0fc0c2c4
sabir kazi <sabirkazi135@gmail.com>
2025-06-21 16:17:15 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 export default function App() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center gap-12 bg-gray-400 p-10 font-mono">
-      <h1 className="text-mint-500 text-3xl font-bold">
+      <h1 className="bg-mint-500 text-3xl font-bold">
         Hello using --color-primary 🎨
       </h1>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,24 @@ import plugin from 'tailwindcss/plugin';
 
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  darkMode: 'class', // enable class-based dark mode
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: 'rgb(var(--color-primary) / <alpha-value>)',
+      },
+    },
   },
-  plugins: [],
+  plugins: [
+    plugin(function ({ addBase }) {
+      addBase({
+        ':root': {
+          '--color-primary': '59 130 246', // blue-500 for light mode
+        },
+        '.dark': {
+          '--color-primary': '147 51 234', // purple-600 for dark mode
+        },
+      });
+    }),
+  ],
 };
