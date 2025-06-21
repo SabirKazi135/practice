859e7d6c6ad85c89debca9951d4e7b4be550cda1
sabir kazi <sabirkazi135@gmail.com>
2025-06-21 18:59:21 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
 export default function App() {
   return (
-    <div className="min-h-screen space-y-6 bg-slate-100 p-10 text-center">
-      <h1 className="text-shadow-lg hover:text-shadow-lg lg:text-shadow-lg">
-        Glowing Text
-      </h1>
+    <div>
+      <h1>Hello World</h1>
     </div>
   );
 }
