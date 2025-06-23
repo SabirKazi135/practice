fcf9cc440ca82d6dc494fab0c461eb1f6f61cbba
sabir kazi <sabirkazi135@gmail.com>
2025-06-23 16:04:44 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 export default function App() {
   return (
     <div>
-      <h1>Hello World</h1>
+      // WITHOUT plugin utility
+      <p className="text-lg font-bold">Normal Text</p>
+      // WITH `.text-shadow-sm`
+      <p className="text-lg font-bold text-shadow-sm">Soft Shadow</p>
+      // WITH `.text-shadow-lg`
+      <p className="text-lg font-bold text-shadow-lg">Big Shadow</p>
+      // WITH `.glow`
+      <div className="glow bg-black p-4 text-white">Glowing Box</div>
     </div>
   );
 }
