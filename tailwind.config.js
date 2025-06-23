85664110449de48376c3027c1e5cf8e72c766ff1
sabir kazi <sabirkazi135@gmail.com>
2025-06-23 16:11:58 +0530

    commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,17 @@ import plugin from 'tailwindcss/plugin';
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        wiggle: {
+          '0%, 100%': { transform: 'rotate(-3deg)' },
+          '50%': { transform: 'rotate(3deg)' },
+        },
+      },
+      animation: {
+        wiggle: 'wiggle 1s ease-in-out infinite',
+      },
+    },
   },
   plugins: [
     plugin(function ({ addUtilities, addComponents, addVariant }) {
