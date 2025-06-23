0ffdbf4e9da31d98ed08a123a7737d5bf7cc8dfb
sabir kazi <sabirkazi135@gmail.com>
2025-06-23 21:56:40 +0530

    commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 // import App from './App.jsx';
 import './index.css';
 
 function App() {
-  const [color, setColor] = useState('red');
+  console.log('direct');
+  useEffect(() => {
+    console.log('inside');
 
-  return (
-    <div className="h-screen bg-slate-600">
-      
-    </div>
-  );
+    return () => {};
+  }, []);
+
+  return <h1> seconds</h1>;
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
