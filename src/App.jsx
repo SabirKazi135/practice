cc82e46a3ce0f9c42fca16aee685cbef398dc492
sabir kazi <sabirkazi135@gmail.com>
2025-06-23 16:09:42 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,7 @@
 export default function App() {
   return (
     <div>
-      <div className="alt">
-        <div className="alt-hover:bg-red-500 p-4 text-white">
-          I change on ALT hover
-        </div>
-      </div>
+      <h1>HEllo</h1>
     </div>
   );
 }
