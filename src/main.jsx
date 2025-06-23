3fabb84ff5c68022fb5ceef6fad765bbe7c6b30d
sabir kazi <sabirkazi135@gmail.com>
2025-06-23 21:37:51 +0530

    commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { useState } from 'react';
 
-import App from './App.jsx';
+// import App from './App.jsx';
 import './index.css';
+
+function App() {
+  const [color, setColor] = useState('red');
+
+  return (
+    <div className="h-screen bg-slate-600">
+      <h1>My favorite color is {color}!</h1>
+      <button type="button" onClick={() => setColor('blue')}>
+        Blue
+      </button>
+      <button type="button" onClick={() => setColor('red')}>
+        Red
+      </button>
+      <button type="button" onClick={() => setColor('pink')}>
+        Pink
+      </button>
+      <button type="button" onClick={() => setColor('green')}>
+        Green
+      </button>
+    </div>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
