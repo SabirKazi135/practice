e1bb43077311547f28c38e2138073a1235ff5f38
sabir kazi <sabirkazi135@gmail.com>
2025-06-26 23:45:06 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,25 @@
 import { useState, useEffect } from 'react';
 
 export default function App() {
-  const [userName, setUserName] = useState('guess');
+  const [count, setCount] = useState(0);
+
   useEffect(() => {
-    console.log('userName changed to ', userName);
-  }, [userName]);
+    console.log('Hello from use useEffect');
+  }, []);
+
+  useEffect(() => {
+    console.log('Counter updated:', count);
+
+    return () => {
+      console.log('switching between two components', count);
+    };
+  }, [count]);
 
   return (
     <div className="flex min-h-screen items-center justify-center bg-gray-100 p-6 text-gray-900 dark:bg-gray-900 dark:text-gray-100">
       <div className="space-x-4">
-        <p>Username: {userName}</p>
-        <button onClick={() => setUserName('Raees')}>Raees</button>
-        <button onClick={() => setUserName('Sabir')}>Sabir</button>
+        <button onClick={() => setCount(count + 1)}>increament</button>
+        <button onClick={() => setCount(count + 1)}>decreament</button>
       </div>
     </div>
   );
