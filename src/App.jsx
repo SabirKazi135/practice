8ed634620b39865fea52e0daf5bee23ebf5d7d16
sabir kazi <sabirkazi135@gmail.com>
2025-06-26 23:24:51 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -9,12 +9,12 @@ export default function App() {
     return () => {};
   }, []);
 
-
-
-
   return (
     <div className="flex min-h-screen items-center justify-center bg-gray-100 p-6 text-gray-900 dark:bg-gray-900 dark:text-gray-100">
-      
+      <div className=''>
+        <button onClick={() => setCount(count + 1)}>increament</button>
+        <button onClick={() => setCount(count + 1)}>decreament</button>
+      </div>
     </div>
   );
 }
