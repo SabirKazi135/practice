f3d867563a9e18e20b2ce6560239e36607fd04d3
sabir kazi <sabirkazi135@gmail.com>
2025-06-26 23:43:16 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import { useState, useEffect } from 'react';
 
 export default function App() {
-  const [userName, setUserName] = useState(0);
+  const [userName, setUserName] = useState('guess');
+  useEffect(() => {
+    console.log('userName changed to ', userName);
+  }, [userName]);
 
   return (
     <div className="flex min-h-screen items-center justify-center bg-gray-100 p-6 text-gray-900 dark:bg-gray-900 dark:text-gray-100">
       <div className="space-x-4">
-        <button onClick={() => changeName('Raees')}>Raees</button>
-        <button onClick={() => changeName('Sabir')}>Sabir</button>
+        <p>Username: {userName}</p>
+        <button onClick={() => setUserName('Raees')}>Raees</button>
+        <button onClick={() => setUserName('Sabir')}>Sabir</button>
       </div>
     </div>
   );
