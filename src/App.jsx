cb697efa6ed0b1c8a43dc3ed6d8b25aa31bc4319
sabir kazi <sabirkazi135@gmail.com>
2025-06-26 23:40:15 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,10 @@ export default function App() {
 
   return (
     <div className="flex min-h-screen items-center justify-center bg-gray-100 p-6 text-gray-900 dark:bg-gray-900 dark:text-gray-100">
-      <button onClick={() => changeToRaees}>Raees</button>
-      <button onClick={() => changeToSabir}>Sabir</button>
+      <div className="space-x-4">
+        <button onClick={() => changeName('Raees')}>Raees</button>
+        <button onClick={() => changeName('Sabir')}>Sabir</button>
+      </div>
     </div>
   );
 }
