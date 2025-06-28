2ee09299b0ac716fa2c81ecf92f48544f86af51b
sabir kazi <sabirkazi135@gmail.com>
2025-06-28 16:58:45 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,33 @@ import { useEffect, useRef, useState } from 'react';
 
 export default function App() {
   // const [number, setNumber] = useState(0);
-  const inputRef = useRef(null);
+  const inputRef1 = useRef(null);
+  const inputRef2 = useRef(null);
+  const inputRef3 = useRef(null);
   useEffect(() => {
     console.log('Component rendered');
   });
 
-  function handleClick() {}
+  function handleClick1() {
+    inputRef.current.focus();
+    inputRef.current.style.backgroundColor = 'yellow';
+  }
+  function handleClick2() {
+    inputRef.current.focus();
+    inputRef.current.style.backgroundColor = 'yellow';
+  }
+  function handleClick3() {
+    inputRef.current.focus();
+    inputRef.current.style.backgroundColor = 'yellow';
+  }
   return (
     <div className="flex h-screen items-center justify-center bg-gray-900 p-6 text-gray-100">
-      <button onClick={handleClick}>Click Me</button>
-      <input type="text" ref={inputRef} />
+      <button onClick={handleClick1}>Click Me</button>
+      <input type="text" ref={inputRef1} />
+      <button onClick={handleClick2}>Click Me</button>
+      <input type="text" ref={inputRef2} />
+      <button onClick={handleClick3}>Click Me</button>
+      <input type="text" ref={inputRef3} />
     </div>
   );
 }
