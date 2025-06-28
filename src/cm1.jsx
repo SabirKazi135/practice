88c559ac7618b6ee864d66ee9b1ee1e20c7e5fb0
sabir kazi <sabirkazi135@gmail.com>
2025-06-28 09:38:55 +0530

    commit

---
+++ src/cm1.jsx
@@ -0,0 +1,17 @@
+// src/App.jsx
+import { createContext, useEffect, useState } from 'react';
+import Cm2 from './cm2';
+
+export const UserContext = createContext();
+export default function App() {
+  const [user, setUser] = useState('Sabir Kazi');
+
+  return (
+    <div className="min-h-screen bg-[#0f0f1a] p-6 text-white">
+      <UserContext.Provider value={user}>
+        <h1>Hello {user} from stage 1</h1>
+        <Cm2 />
+      </UserContext.Provider>
+    </div>
+  );
+}
--- src/cm3.jsx
+++ src/cm3.jsx
@@ -1,9 +1,13 @@
+import { useContext } from 'react';
+import { UserContext } from './App.jsx';
 function Cm3() {
-    return (
-        <div>
-            <h1>Hello from stage 3</h1>
-        </div>
-    );
+  const user3 = useContext(UserContext);
+
+  return (
+    <div>
+      <h1>Hello {user3} from stage 3</h1>
+    </div>
+  );
 }
 
 export default Cm3;
