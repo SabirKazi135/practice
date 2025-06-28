9fb535e334a6d9ef57022e19934af9eb28ac8e0e
sabir kazi <sabirkazi135@gmail.com>
2025-06-28 16:49:52 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,14 @@ import { useEffect, useRef, useState } from 'react';
 export default function App() {
   const [number, setNumber] = useState(0);
 
+  
+
+  function handleClick() {
+    setNumber((n) => n + 1);
+  }
   return (
     <div className="flex h-screen items-center justify-center bg-gray-900 p-6 text-gray-100">
-      <button>Click Me</button>
+      <button onClick={handleClick}>Click Me</button>
     </div>
   );
 }
