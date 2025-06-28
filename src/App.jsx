0c6952c80dcf50c9a71b3a144b286ba0716c7f81
sabir kazi <sabirkazi135@gmail.com>
2025-06-28 18:43:18 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,6 @@
 export default function App() {
   return (
-    <div
-      className={`flex h-screen flex-col items-center justify-center gap-5 ${isDarkMode ? 'bg-gray-900 text-gray-100' : 'bg-gray-100 text-gray-900'} p-6`}
-    >
-      <div className="text-4xl font-bold text-red-500"> Count: {count}</div>
+    <div className="flex h-screen flex-col items-center justify-center gap-5 bg-gray-900 p-6 text-gray-100">
       <h1>Hello World</h1>
     </div>
   );
