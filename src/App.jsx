9c71f8968313673e4c1d5fbf8c3ea7a365e43bc7
sabir kazi <sabirkazi135@gmail.com>
2025-06-28 17:23:26 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import { useEffect, useRef, useState } from 'react';
-
+import { MyContext } from './context.js';
 export default function App() {
+  const [isDarkMode, setDarkMode] = useState(true);
   return (
-    <div className="flex h-screen flex-col items-center justify-center bg-gray-900 p-6 text-gray-100">
-      
-    </div>
+    <MyContext.prototype
+      value={{ isDarkMode, setDarkMode }}
+      className={`flex h-screen flex-col items-center justify-center ${isDarkMode ? 'bg-gray-900 text-gray-100' : 'bg-gray-100 text-gray-900'} p-6`}
+    >
+      <h1>Hello</h1>
+    </MyContext.prototype>
   );
 }
