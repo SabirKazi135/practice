05583ff25f398117dd8b0a0b801d64f4e6e58008
sabir kazi <sabirkazi135@gmail.com>
2025-06-28 16:54:59 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,16 @@ import { useEffect, useRef, useState } from 'react';
 
 export default function App() {
   // const [number, setNumber] = useState(0);
-  const ref = useRef(0);
-
+  const inputRef = useRef(null);
   useEffect(() => {
     console.log('Component rendered');
   });
 
-  function handleClick() {
-    ref.current++;
-  }
+  function handleClick() {}
   return (
     <div className="flex h-screen items-center justify-center bg-gray-900 p-6 text-gray-100">
       <button onClick={handleClick}>Click Me</button>
+      <input type="text" ref={inputRef} />
     </div>
   );
 }
