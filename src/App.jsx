7807dd495296e6794683d543416b48ec913e7653
sabir kazi <sabirkazi135@gmail.com>
2025-06-28 10:02:57 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -7,3 +7,13 @@ export default function App() {
     </div>
   );
 }
+
+import Cm0 from './cm0';
+
+export default function App() {
+  return (
+    <div className="min-h-screen bg-[#0f0f1a] p-6 text-white">
+      <Cm0 />
+    </div>
+  );
+}
