30dad227b60f28d6dede52b8cf77860cdaa393e0
sabir kazi <sabirkazi135@gmail.com>
2025-06-30 01:09:03 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useRef, useEffect, useState } from 'react';
 function App() {
   const input = useRef(null);
   useEffect(() => {
-    input.current.focus();
+    input.current?.focus();
   });
 
   return (
@@ -10,6 +10,7 @@ function App() {
       <input
         type="text"
         placeholder="Enter text..."
+        ref={input}
         className="rounded-md bg-gray-400 px-5 py-2 text-base text-gray-100 placeholder:text-gray-100 focus:outline-none focus:ring-2 focus:ring-yellow-400"
       />
     </div>
