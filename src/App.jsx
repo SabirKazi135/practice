60e854b73cdcd42dfa8a3a62b93fa4d1cbda7b83
sabir kazi <sabirkazi135@gmail.com>
2025-06-30 01:32:10 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
 import { useRef, useEffect, useState, useReducer } from 'react';
+
+function reducer(state, action) {
+  
+}
 function App() {
   const [state, depatch] = useReducer();
+
+
   return (
-    <div className="flex h-screen flex-col items-center justify-center gap-5 bg-gray-900 p-6 text-gray-100"></div>
+    <div className="flex h-screen flex-col items-center justify-center gap-5 bg-gray-900 p-6 text-gray-100">
+
+    </div>
   );
 }
 
