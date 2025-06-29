687d67608a437ca8d441936afe8c3911d9f4bd4f
sabir kazi <sabirkazi135@gmail.com>
2025-06-30 01:21:38 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,7 @@
 import { useRef, useEffect, useState } from 'react';
 function App() {
-  const input = useRef(null);
-  const click = useRef(0);
-  const [count, setCount] = useState(0);
-  const countRef = useRef(null);
-  useEffect(() => {
-    input.current?.focus();
-  });
-
-  function clicked() {
-    click.current++;
-    console.log('Button CLiked : ', click.current);
-  }
-
-  function increament() {
-    countRef.current = count;
-    setCount((c) => c + 1);
-    console.log('Ref count: ', countRef.current);
-  }
   return (
-    <div className="flex h-screen flex-col items-center justify-center gap-5 bg-gray-900 p-6 text-gray-100">
-      <input
-        type="text"
-        placeholder="Enter text..."
-        ref={input}
-        className="rounded-md bg-gray-400 px-5 py-2 text-base text-gray-100 placeholder:text-gray-100 focus:outline-none focus:ring-2 focus:ring-yellow-400"
-      />
-
-      <button
-        className="rounded-[6px] bg-blue-400 px-4 py-1 text-xl"
-        onClick={() => clicked()}
-      >
-        Click
-      </button>
-      <p>Count: {count}</p>
-      <button onClick={() => increament()}>Increament</button>
-    </div>
+    <div className="flex h-screen flex-col items-center justify-center gap-5 bg-gray-900 p-6 text-gray-100"></div>
   );
 }
 
