2f884f557408e00734e6e0f0a52f45294b1cf289
sabir kazi <sabirkazi135@gmail.com>
2025-06-30 01:11:11 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import { useRef, useEffect, useState } from 'react';
 function App() {
   const input = useRef(null);
+  const click = useRef(0);
   useEffect(() => {
     input.current?.focus();
   });
 
+  function clicked(\) {
+    click.current++
+    console.log("Button CLiked : ", click.current);
+  }
   return (
     <div className="flex h-screen flex-col items-center justify-center gap-5 bg-gray-900 p-6 text-gray-100">
       <input
@@ -13,6 +18,8 @@ function App() {
         ref={input}
         className="rounded-md bg-gray-400 px-5 py-2 text-base text-gray-100 placeholder:text-gray-100 focus:outline-none focus:ring-2 focus:ring-yellow-400"
       />
+
+      <button onClick={() => clicked()}>Click</button>
     </div>
   );
 }
