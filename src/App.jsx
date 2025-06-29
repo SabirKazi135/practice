342c17ba635782af83a343fcb9c7e5a9a60e69e0
sabir kazi <sabirkazi135@gmail.com>
2025-06-30 01:08:29 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
 import { useRef, useEffect, useState } from 'react';
 function App() {
+  const input = useRef(null);
+  useEffect(() => {
+    input.current.focus();
+  });
+
   return (
     <div className="flex h-screen flex-col items-center justify-center gap-5 bg-gray-900 p-6 text-gray-100">
       <input
