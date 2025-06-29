f6c08db5c22eb262b1e8caf2dc201d17b3f1bf73
sabir kazi <sabirkazi135@gmail.com>
2025-06-30 01:06:10 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,11 @@ import { useRef, useEffect, useState } from 'react';
 function App() {
   return (
     <div className="flex h-screen flex-col items-center justify-center gap-5 bg-gray-900 p-6 text-gray-100">
-      
+      <input
+        type="text"
+        placeholder="Enter text..."
+        className="rounded-md bg-gray-400 px-5 py-2 text-base text-gray-100 placeholder:text-gray-100 focus:outline-none focus:ring-2 focus:ring-yellow-400"
+      />
     </div>
   );
 }
