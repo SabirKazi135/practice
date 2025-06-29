245487a590d36b915d69ca1119936c57188a9ed8
sabir kazi <sabirkazi135@gmail.com>
2025-06-30 01:29:08 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import { useRef, useEffect, useState, useReducer } from 'react';
 function App() {
   return (
-    <div className="flex h-screen flex-col items-center justify-center gap-5 bg-gray-900 p-6 text-gray-100"></div>
+    <div className="flex h-screen flex-col items-center justify-center gap-5 bg-gray-900 p-6 text-gray-100">
+      
+    </div>
   );
 }
 
