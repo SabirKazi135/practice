d976bfd315fc5562ac5f3a96893044eccacc6697
sabir kazi <sabirkazi135@gmail.com>
2025-07-02 21:46:58 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Home } from './Components/Home';
 import { About } from './Components/About';
 import { NavBar } from './Components/NavBar';
 import OrderSummary from './Components/OrderSummary';
+import NoMatch from './Components/NoMatch';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/order-summary" element={<OrderSummary />} />
+        <Route path="*" element={<NoMatch />} />
       </Routes>
     </>
   );
