bc02b5409754c64973dcec263ebd75b857344a62
sabir kazi <sabirkazi135@gmail.com>
2025-07-02 20:12:40 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
+import { Routes, Route } from 'react-router-dom';
+import { Home } from './Components/Home';
+import { About } from './Components/About';
+
 function App() {
   return (
-    <div>
-      
-      </div>
-    );
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+    </Routes>
+  );
 }
 
 export default App;
