3fd51625b70b60c8992c38ed60e982cbb0be3954
sabir kazi <sabirkazi135@gmail.com>
2025-07-02 18:23:44 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,9 @@
-import Counter from './components/Counter';
+import { Home } from 'lucide-react';
 
 export default function App() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-gray-900 p-6 text-white">
-      <div className="flex flex-col items-center bg-gray-100 p-6">
-        <h2 className="mb-4 text-2xl font-bold text-gray-800">
-          Redux Counter App
-        </h2>
-        <Counter />
-      </div>
-    </div>
+    <>
+      <Home />
+    </>
   );
 }
