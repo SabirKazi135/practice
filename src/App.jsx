0828c593a0c090a8c9f9f71d8fe07b8db2177d47
sabir kazi <sabirkazi135@gmail.com>
2025-07-02 19:58:50 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
-import { Home } from 'lucide-react';
+import { Routes, Route } from 'react-router-dom';
+import BottomNav from './components/navigation/BottomNav';
+import Home from './pages/Home';
 
 export default function App() {
   return (
-    <>
-      <Home />
-    </>
+    <div className="aspect-[383/768] h-[768px] w-[383px] overflow-hidden bg-gray-50 pb-20">
+      <Routes>
+        <Route path="/daily-tracker" element={<Home />} />
+        <Route path="/meals" element={<div>Meals</div>} />
+        <Route path="/add" element={<div>Add</div>} />
+        <Route path="/ingredients" element={<div>Ingredients</div>} />
+        <Route path="/dashboard" element={<div>Dashboard</div>} />
+      </Routes>
+      <BottomNav />
+    </div>
   );
 }
