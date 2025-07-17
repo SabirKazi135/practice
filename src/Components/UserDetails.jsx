5f371b02ec00a660b2d14921723963494ad2d587
sabir <sabir@root.root>
2025-07-17 16:48:51 +0530

    commit

--- src/Components/UserDetails.jsx
+++ src/Components/UserDetails.jsx
@@ -1,9 +1,11 @@
-import { useParams } from "react-router-dom";
+import { useParams } from 'react-router-dom';
 
 function UserDetails() {
+  const params = useParams();
+  const userId = params.userId;
   return (
     <div>
-      <h1>Details about user</h1>
+      <h1>Details about user {userId}</h1>
     </div>
   );
 }
