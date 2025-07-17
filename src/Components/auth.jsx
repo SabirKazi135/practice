490905c92ae5d9694844313f4f4a4d1bea4f8c5d
sabir <sabir@root.root>
2025-07-17 17:11:25 +0530

    commit

--- src/Components/auth.jsx
+++ src/Components/auth.jsx
@@ -1,6 +1,18 @@
-import { createContext } from "react";
+import { Children, createContext } from 'react';
 
 const AuthContext = createContext(null);
 export function AuthProvider() {
-    const [user, setUser] = useState(null);
+  const [user, setUser] = useState(null);
+  const login = (user) => {
+    setUser(user);
+  };
+  const logout = () => {
+    setUser(null);
+  };
+
+  return (
+    <AuthContext.Provider value={(user, login, logout)}>
+      {Children}
+    </AuthContext.Provider>
+  );
 }
