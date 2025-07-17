fb5b20907286980e69f719b10c49ec2c5b38e4a6
sabir <sabir@root.root>
2025-07-17 20:24:33 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
 import { Route, Router } from 'react-router-dom';
+import Home from './pages/Home';
+import Contact from './pages/Contact';
+import About from './pages/About';
 function App() {
   return (
     <div>
-          <div>This is Home page</div>
-          
+      <div>This is Home page</div>
+      <Router>
+        <Route path="/" element={<Home />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/about" element={<About />} />
+      </Router>
     </div>
   );
 }
