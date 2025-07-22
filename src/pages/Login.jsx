b203783273f707b309e538f03447f94ce6f10da6
sabir <sabir@root.root>
2025-07-22 08:33:15 +0530

    commit

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,15 @@
+import { useState } from 'react';
 export default function Login() {
-    
+  const [userName, setUserName] = useState(null);
   return (
     <div>
       <h2>Login Page</h2>
-      <input type="text" placeholder="Enter username" value={userName}/>
+      <input
+        type="text"
+        placeholder="Enter username"
+        onChange={(e) => setUserName(e.target.value)}
+        value={userName}
+      />
     </div>
   );
 }
