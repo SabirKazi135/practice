29e6b34041159eed2225dc33b45a8bfb4d53bef9
sabir <sabir@root.root>
2025-07-22 08:37:14 +0530

    commit

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,15 @@
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 export default function Login() {
+    const Navigate = useNavigate();
   const [userName, setUserName] = useState(null);
-  
+  function Login() {
+    if (userName == "sabir") {
+        
+    } else {
+        
+    }
+  }
   return (
     <div>
       <h2>Login Page</h2>
