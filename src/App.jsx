8275a1b61b2c1d4f2376ca68b6a1643e0b513b13
sabirkazi135 <sabirkazi135@gmail.com>
2025-08-10 15:17:09 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
-function App() {
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
+
+export default function App() {
   return (
-    <div>
-      <h1>Hello World Sabir</h1>
-    </div>
+    <BrowserRouter>
+      <nav>
+        {/* Link prevents reload */}
+        <Link to="/">Home</Link> | <Link to="/about">About</Link>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
-
-export default App;
