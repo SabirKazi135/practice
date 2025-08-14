25472c14192c3f11760a74c9ce61b767e6de52bc
sabirkazi135 <sabirkazi135@gmail.com>
2025-08-14 21:07:28 +0530

    commit

--- src/pages/UserDetails.jsx
+++ src/pages/UserDetails.jsx
@@ -1,10 +1,15 @@
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 function UserDetails() {
   const { userName } = useParams();
+  const navigate = useNavigate();
+  function GoBack() {
+    navigate(-1);
+  }
   return (
     <>
       <h2>Extra Details for {userName}</h2>
       <p>This section contains additional information about the {userName}.</p>
+      <button onClick={() => GoBack()}>Go Back</button>
     </>
   );
 }
