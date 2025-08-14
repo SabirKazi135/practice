3fe3f91fecd538396bab76e1e8b910352cfd515c
sabirkazi135 <sabirkazi135@gmail.com>
2025-08-14 16:27:30 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Route, Router, NavLink } from 'react-router-dom';
+import { Route, NavLink, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
 import Contact from './pages/Contact';
@@ -9,24 +9,39 @@ function App() {
     <>
       <h1>Main Page </h1>
       <h2>Main nav Page</h2>
+
       <nav>
-        <li>
-          <NavLink to="/">Home</NavLink>
-          <NavLink to="/about">About</NavLink>
-          <NavLink to="/contact">Contact</NavLink>
-          <NavLink to="/user/:sabir">sabir profile</NavLink>
-          <NavLink to="/user/:alice">alice profile</NavLink>
-          <NavLink to="/product/:1">prodcut 2</NavLink>
-          <NavLink to="/product/:2">product 2</NavLink>
-        </li>
+        <ul>
+          <li>
+            <NavLink to="/">Home</NavLink>
+          </li>
+          <li>
+            <NavLink to="/about">About</NavLink>
+          </li>
+          <li>
+            <NavLink to="/contact">Contact</NavLink>
+          </li>
+          <li>
+            <NavLink to="/user/sabir">Sabir Profile</NavLink>
+          </li>
+          <li>
+            <NavLink to="/user/alice">Alice Profile</NavLink>
+          </li>
+          <li>
+            <NavLink to="/product/1">Product 1</NavLink>
+          </li>
+          <li>
+            <NavLink to="/product/2">Product 2</NavLink>
+          </li>
+        </ul>
       </nav>
-      <Router>
+      <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
-        <Route path="/Contact" element={<Contact />} />
+        <Route path="/contact" element={<Contact />} />
         <Route path="/user/:userName" element={<User />} />
         <Route path="/product/:productId" element={<Product />} />
-      </Router>
+      </Routes>
     </>
   );
 }
