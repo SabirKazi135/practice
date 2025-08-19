e112385620cede45233f855be9edbc1282461023
sabirkazi135 <sabirkazi135@gmail.com>
2025-08-20 01:27:17 +0530

    commit

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,7 +3,7 @@ function About() {
     <div className="space-y-4">
       <h2 className="text-2xl font-semibold">This is About Page</h2>
       <p className="text-gray-300">
-        Learn more about us here. We are building something amazing!
+        Learn more about us here. check We are building something amazing!
       </p>
     </div>
   );
