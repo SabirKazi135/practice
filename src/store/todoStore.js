f7e36bb179baba25f63cd0fa68096dcff011dd1d
sabirkazi135 <sabirkazi135@gmail.com>
2025-09-22 16:34:47 +0530

    commit

--- src/store/todoStore.js
+++ src/store/todoStore.js
@@ -1,2 +1,23 @@
-import { create } from "zustand";
+import { create } from 'zustand';
 
+const useTodoStore = create((set) => ({
+  todos: [],
+
+  addTodo: (id, text) => {
+    const newTodo = { id: id, text, done: false };
+    set((state) => ({ todos: [...state.todos, newTodo] }));
+  },
+
+  removeTodo: (id) =>
+    set((state) => ({ todos: state.todos.filter((todo) => todo.id !== id) })),
+
+  toggleTodo: (id) => {
+    set((state) => ({
+      todos: state.todos.map((todo) =>
+        todo.id === id ? { ...todo, done: !todo.done } : todo,
+      ),
+    }));
+  },
+}));
+
+export default useTodoStore;
