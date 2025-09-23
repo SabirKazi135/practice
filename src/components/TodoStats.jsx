35b8204b397996d81db4e97aefacfb6ed54bc16f
sabirkazi135 <sabirkazi135@gmail.com>
2025-09-23 17:40:34 +0530

    practice 1

--- src/components/TodoStats.jsx
+++ src/components/TodoStats.jsx
@@ -2,12 +2,15 @@ import useTodoStore from '../store/todoStore';
 
 function TodoStats() {
   const completedCount = useTodoStore((state) => state.completedCount());
+  const pendingCount = useTodoStore((state) => state.pendingCount());
   const totalTodos = useTodoStore((state) => state.todos.length);
 
   return (
-    <div>
-      <p>
-        Completed {completedCount} of {totalTodos} todos
+    <div className="mt-6 w-full rounded-lg bg-white p-4 shadow-md">
+      <p className="text-center text-lg font-semibold text-gray-700">
+        ✅ Completed <span className="text-green-600">{completedCount}</span> of{' '}
+        <span className="text-blue-600">{totalTodos}</span> todos and pending{' '}
+        <span>{pendingCount}</span>
       </p>
     </div>
   );
--- src/store/todoStore.js
+++ src/store/todoStore.js
@@ -1,25 +1,38 @@
 import { create } from 'zustand';
+import { persist, devtools } from 'zustand/middleware';
 
-const useTodoStore = create((set, get) => ({
-  todos: [],
+const useTodoStore = create(
+  devtools(
+    persist((set, get) => ({
+      todos: [
+        { id: 1, text: 'Learn Zustand basics', done: false },
+        { id: 2, text: 'Build Todo App', done: true },
+        { id: 3, text: 'Add Tailwind styling', done: false },
+        { id: 4, text: 'Share project in public', done: false },
+      ],
 
-  addTodo: (id, text) => {
-    const newTodo = { id, text, done: false };
-    set((state) => ({ todos: [...state.todos, newTodo] }));
-  },
+      addTodo: (id, text) => {
+        const newTodo = { id, text, done: false };
+        set((state) => ({ todos: [...state.todos, newTodo] }));
+      },
 
-  removeTodo: (id) =>
-    set((state) => ({ todos: state.todos.filter((todo) => todo.id !== id) })),
+      removeTodo: (id) =>
+        set((state) => ({
+          todos: state.todos.filter((todo) => todo.id !== id),
+        })),
 
-  toggleTodo: (id) => {
-    set((state) => ({
-      todos: state.todos.map((todo) =>
-        todo.id === id ? { ...todo, done: !todo.done } : todo,
-      ),
-    }));
-  },
+      toggleTodo: (id) => {
+        set((state) => ({
+          todos: state.todos.map((todo) =>
+            todo.id === id ? { ...todo, done: !todo.done } : todo,
+          ),
+        }));
+      },
 
-  completedCount: () => get().todos.filter((t) => t.done).length,
-}));
+      completedCount: () => get().todos.filter((t) => t.done).length,
+      pendingCount: () => get().todos.filter((t) => t.done === false).length,
+    })),
+  ),
+);
 
 export default useTodoStore;
