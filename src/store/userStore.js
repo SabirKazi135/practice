82d8e2a510d4cdf6fd8c79197fb04f4a626ff0ac
sabirkazi135 <sabirkazi135@gmail.com>
2025-09-26 16:01:18 +0530

    commit

--- src/store/userStore.js
+++ src/store/userStore.js
@@ -1,2 +1,21 @@
-import { create } from "zustand";
+import { create } from 'zustand';
 
+const useUserStore = create((set) => ({
+  users: [],
+  loading: false,
+  error: null,
+
+  fetchUsers: async () => {
+    set({
+      loading: true,
+      error: null,
+    });
+    try {
+      const res = await fetch('https://jsonplaceholder.typicode.com/users');
+      const data = await res.json();
+      set({ user: data, loading: false });
+    } catch (err) {
+      set({ error: err.message, loading: false });
+    }
+  },
+}));
